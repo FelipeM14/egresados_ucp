2f6cc09b1d273540cbaf6b94a005b87732caef75
Juan_GGG <dankomaria@hotmail.com>
2019-08-11 17:57:42 -0500

    Add auth

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,13 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+//mix.js('resources/js/app.js', 'public/js')
+    //.sass('resources/sass/app.scss', 'public/css');
+
+mix.js([
+    'resources/js/js_template/jquery/jquery-2.2.4.min.js',
+    'resources/js/js_template/bootstrap/popper.min.js',
+    'resources/js/js_template/bootstrap/bootstrap.min.js',
+    'resources/js/js_template/active.js'
+], 'public/js/out_template.js').
+style(['resources/css/style.css'],'public/css/out_template.css');
