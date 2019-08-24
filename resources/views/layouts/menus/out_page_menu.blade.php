98d9d04d537e0a86889c47fff8bd6a35f6b0222d
mrcrowley2291 <fabian1.naranjo@ucp.edu.co>
2019-08-24 18:15:19 -0500

    nuevo cambio de interface de inicio

--- resources/views/layouts/menus/out_page_menu.blade.php
+++ resources/views/layouts/menus/out_page_menu.blade.php
@@ -3,7 +3,7 @@
         <li><a href="index.html">Inicio</a></li>
         <li><a href="#" data-toggle="modal" data-target="#mimodal">Tarjeta Profesional</a></li>
         <li><a href="https://www.elempleo.com/sitios-empresariales/colombia/universidad-catolica-pereira/index.asp">Bolsa De Empleo</a></li>
-        <li><a href="encuestaG.html">Encuesta Graduados</a></li>
+        <li><a href="https://docs.google.com/a/ucp.edu.co/forms/d/e/1FAIpQLScuaS2FyzFat18maFTwjdBTGMGJeIwMU91oatJBfpISsJdrcg/viewform">Encuesta Graduados</a></li>
 
     </ul>
 </div>
