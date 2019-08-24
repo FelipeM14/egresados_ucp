07c1005063e021d340314ead86abc1ccc61d33d5
Juan_GGG <dankomaria@hotmail.com>
2019-08-24 15:36:53 -0500

    Add mix manifest

--- resources/views/layouts/menus/left_app_menu.blade.php
+++ resources/views/layouts/menus/left_app_menu.blade.php
@@ -3,9 +3,9 @@
         <div class="header__pane ml-auto">
             <div>
                 <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
-                                    <span class="hamburger-box">
-                                        <span class="hamburger-inner"></span>
-                                    </span>
+                    <span class="hamburger-box">
+                        <span class="hamburger-inner"></span>
+                    </span>
                 </button>
             </div>
         </div>
@@ -13,20 +13,20 @@
     <div class="app-header__mobile-menu">
         <div>
             <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
-                                <span class="hamburger-box">
-                                    <span class="hamburger-inner"></span>
-                                </span>
+                <span class="hamburger-box">
+                    <span class="hamburger-inner"></span>
+                </span>
             </button>
         </div>
     </div>
     <div class="app-header__menu">
-                        <span>
-                            <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
-                                <span class="btn-icon-wrapper">
-                                    <i class="fa fa-ellipsis-v fa-w-6"></i>
-                                </span>
-                            </button>
-                        </span>
+        <span>
+            <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
+                <span class="btn-icon-wrapper">
+                    <i class="fa fa-ellipsis-v fa-w-6"></i>
+                </span>
+            </button>
+        </span>
     </div>
 
     <div class="scrollbar-sidebar">
@@ -97,37 +97,13 @@
                         </li>
                     </ul>
                 </li>
-                <li
-
-
-
-
-                    class="mm-active"
-
-
-
-
-
-
-                >
+                <li class="mm-active">
                     <a href="#">
                         <i class="metismenu-icon pe-7s-car"></i>
                         Components
                         <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
                     </a>
-                    <ul
-
-
-
-
-                        class="mm-show"
-
-
-
-
-
-
-                    >
+                    <ul class="mm-show">
                         <li>
                             <a href="components-tabs.html">
                                 <i class="metismenu-icon">
