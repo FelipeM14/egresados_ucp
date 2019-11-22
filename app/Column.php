22a0ff14fa164aca4c365e131aee2413811b91d7
Angelicamrc <angelica1.ramirez@ucp.edu.co>
2019-11-21 20:59:07 -0500

    se eliminan clases inesesarias

--- app/Column.php
+++ app/Column.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Column extends Model
 {
     protected $fillable = ['title', 'name', 'order', 'category_id', 'status', 'size'];
-
+    //trae la categoria de una columna
     public function categories(){
         return $this->belongsTo('App\Category');
     }
-
+    //trae las opciones de una columna
     public function options(){
         return $this->hasMany('App\Option');
     }
